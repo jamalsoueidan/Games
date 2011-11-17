438e481c69caabf43e467c19a6acb186496805e9
Jamal Soueidan <jamal@soueidan.com>
2011-11-17 11:49:24 +0100

    Updated Facebook API keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, '2ECv3dt6WSAw01rj1OX0A', '1mbKp47H6KMLtddhf0ct9nX1TcJWxpXRuEerK5Cggw'
-  provider :facebook, '167389690004850', '205443f29a5ea8b9a91e4851874f2f66', {:scope => 'email, offline_access'}
+  provider :facebook, '317584588267521', '1300417f35c090abfe0080efd8136227', {:scope => 'email, offline_access'}
   provider :linked_in, 'dkwf0xra1gs0', 'ZmcAqtnlPQQk7cvZ'  
  
   # generic openid
